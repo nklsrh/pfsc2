ee2df22fc3be40064223a1dced81cd741bf6a9c3
Nikhil Suresh <nklsrh@gmail.com>
2017-05-22 23:10:45 +1000

    + Buyout PlayerToken

--- supercricket2-cloudscript.js
+++ supercricket2-cloudscript.js
@@ -1,15 +1,16 @@
 // defining these up top so we can easily change these later if we need to.
-var TICKET_CURRENCY = "TK";					  // currecny code for our Tickets VC
-var GOLD_CURRENCY = "GL";					  // currecny code for our Tickets VC
-var PLAYERTOKENS_CURRENCY = "PT";					  // currecny code for our Tickets VC
+var TICKET_CURRENCY = "TK"; // currecny code for our Tickets VC
+var GOLD_CURRENCY = "GL"; // currecny code for our Tickets VC
+var PLAYERTOKENS_CURRENCY = "PT"; // currecny code for our Tickets VC
 
-handlers.PlayMatchTickets = function(args) {
+handlers.PlayMatchTickets = function(args)
+{
 	// get the calling player's inventory and VC balances
 	var GetUserInventoryRequest = {
-        "PlayFabId": currentPlayerId
-    };
+		"PlayFabId": currentPlayerId
+	};
 
-     var GetUserInventoryResult = server.GetUserInventory(GetUserInventoryRequest);
+	var GetUserInventoryResult = server.GetUserInventory(GetUserInventoryRequest);
 	var userInventory = GetUserInventoryResult.Inventory;
 	var userVcBalances = GetUserInventoryResult.VirtualCurrency;
 	var userVcRecharge = GetUserInventoryResult.VirtualCurrencyRechargeTimes;
@@ -17,17 +18,17 @@ handlers.PlayMatchTickets = function(args) {
 	// make sure the player has > 0 Tickets before proceeding. 
 	try
 	{
-		if(!CheckTickets(userVcBalances))
+		if (!CheckTickets(userVcBalances))
 		{
 			throw "No Tickets remaining. Purchase additional Tickets or wait: " + userVcRecharge[TICKET_CURRENCY].SecondsToRecharge + " seconds.";
 		}
 	}
-	catch(ex)
+	catch (ex)
 	{
 		return JSON.stringify(ex);
 	}
-	
-	
+
+
 	var ticketsSpent = 1;
 	if (args && args.hasOwnProperty("tp"))
 	{
@@ -49,7 +50,7 @@ handlers.PlayMatchTickets = function(args) {
 	}
 
 
-	
+
 	var playerTokensEarned = 0;
 	if (args && args.hasOwnProperty("pt"))
 	{
@@ -61,21 +62,54 @@ handlers.PlayMatchTickets = function(args) {
 	}
 
 
-	
+
 	SubtractVc(userVcBalances, TICKET_CURRENCY, ticketsSpent);
-	
+
 	var results = {};
-		results.ts = ticketsSpent;
-		results.ge = goldEarned;
-		results.pte = playerTokensEarned;
+	results.ts = ticketsSpent;
+	results.ge = goldEarned;
+	results.pte = playerTokensEarned;
+
+	return JSON.stringify(results);
+};
+
+handlers.BuyoutPlayerTokens = function(args)
+{
+	// get the calling player's inventory and VC balances
+	var GetUserInventoryRequest = {
+		"PlayFabId": currentPlayerId
+	};
+
+	var GetUserInventoryResult = server.GetUserInventory(GetUserInventoryRequest);
+	var userVcBalances = GetUserInventoryResult.VirtualCurrency;
+
+	// Check how much is the playeroverall, use that as a guide to generate cost
+	var playerOverall = 50;
+	if (args && args.hasOwnProperty("po"))
+	{
+		if (args.po > playerOverall)
+		{
+			playerOverall = args.po;
+		}
+	}
+
+	var amountRequired = playerOverall * 15;
+
+	if (HasEnough(userVcBalances, GOLD_CURRENCY, amountRequired))
+	{
+		SubtractVc(userVcBalances, GOLD_CURRENCY, amountRequired);
+	}
+
+	var results = {};
+	results.earnd = true;
 
 	return JSON.stringify(results);
 };
 
 
-function CheckTickets(vcBalnces)
+function HasEnough(vcBalnces, currency, required)
 {
-	if(vcBalnces != null && vcBalnces.hasOwnProperty(TICKET_CURRENCY) && vcBalnces[TICKET_CURRENCY] > 0)
+	if (vcBalnces != null && vcBalnces.hasOwnProperty(currency) && vcBalnces[currency] >= required)
 	{
 		return true;
 	}
@@ -85,33 +119,38 @@ function CheckTickets(vcBalnces)
 	}
 }
 
+function CheckTickets(vcBalnces)
+{
+	return HasEnough(vcBalnces, TICKET_CURRENCY, 1);
+}
+
 function AddVc(vcBalnces, code, qty)
-{ 
-	if(vcBalnces != null && vcBalnces.hasOwnProperty(code) &&  vcBalnces[code] > 0)
+{
+	if (vcBalnces != null && vcBalnces.hasOwnProperty(code) && vcBalnces[code] > 0)
 	{
 		vcBalnces[code] += qty;
 	}
 
 	var AddUserVirtualCurrencyRequest = {
-	    "PlayFabId" : currentPlayerId,
-	    "VirtualCurrency": code,
-	    "Amount": qty
-    };
-    var AddUserVirtualCurrencyResult = server.AddUserVirtualCurrency(AddUserVirtualCurrencyRequest);
+		"PlayFabId": currentPlayerId,
+		"VirtualCurrency": code,
+		"Amount": qty
+	};
+	var AddUserVirtualCurrencyResult = server.AddUserVirtualCurrency(AddUserVirtualCurrencyRequest);
 }
 
 function SubtractVc(vcBalnces, code, qty)
 {
-	if(vcBalnces != null && vcBalnces.hasOwnProperty(code) &&  vcBalnces[code] > 0)
+	if (vcBalnces != null && vcBalnces.hasOwnProperty(code) && vcBalnces[code] > 0)
 	{
 		vcBalnces[code] -= qty;
 	}
 
 	var SubtractUserVirtualCurrencyRequest = {
-	    "PlayFabId" : currentPlayerId,
-	    "VirtualCurrency": code,
-	    "Amount": qty
-    };
+		"PlayFabId": currentPlayerId,
+		"VirtualCurrency": code,
+		"Amount": qty
+	};
 
-    var SubtractUserVirtualCurrencyResult = server.SubtractUserVirtualCurrency(SubtractUserVirtualCurrencyRequest);
+	var SubtractUserVirtualCurrencyResult = server.SubtractUserVirtualCurrency(SubtractUserVirtualCurrencyRequest);
 }
